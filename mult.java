36379a08a9ac9f732a71b9f204beb1f334930674
pranathi97 <pranathi0297@gmail.com>
2019-02-06 10:42:25 -0800

    build

--- mult.java
+++ mult.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class mult{
+public class Mult{
    public static void main(String[] args) {
       Scanner scan = new Scanner(System.in);
       System.out.print("Enter first number: ");
